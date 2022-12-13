a0f8d78194e57b96cd69d37f81ed85f7ec078d3a
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2022-12-13 14:08:26 +0900

    Update config.php

--- shourin-quest/app/src/config.php
+++ shourin-quest/app/src/config.php
@@ -1,21 +1,4 @@
 <?php
-// $uri = $_SERVER['REQUEST_URI'];
-// echo $_SERVER['REQUEST_URI'];
-
-// if (preg_match("/(.+(src))/i", $uri, $match)) {
-//     define('BASE_CONTEXT_PATH', $match[0] . '/');
-// }
-
-
-// define('BASE_IMAGE_PATH', BASE_CONTEXT_PATH . 'images/');
-// define('BASE_JS_PATH', BASE_CONTEXT_PATH . 'js/');
-// define('BASE_CSS_PATH', BASE_CONTEXT_PATH . 'css/');
-
-// define('BASE_CONTEXT_PATH', );
-
-
-// $uri = $_SERVER['REQUEST_URI'];
-// echo $url;
 
 define('CURRENT_URI', $_SERVER['REQUEST_URI']);
 
